0b4ee3a9e5613208fc94a5d7c890b034ddebec4b
Kevin Bohry <kevinportella@gmail.com>
2021-07-09 15:08:24 -0300

    Challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,18 +12,40 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState(''); //input
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle.trim() === '') return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    setTasks(task => [...task, newTask])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+
+      ...task,
+      isComplete: !task.isComplete
+      
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskFilter = tasks.filter(task => task.id !== id)
+
+    setTasks(taskFilter);
   }
 
   return (
@@ -37,6 +59,12 @@ export function TaskList() {
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
+            onKeyPress = {(event) => {
+              if (event.code === 'Enter') {
+                handleCreateNewTask();
+                setNewTaskTitle('')
+              }
+            }}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
