32784e0114a047ca733c72be9b15d81753a29bfc
Kevin Bohry <kevinportella@gmail.com>
2021-09-22 11:39:07 -0300

    add picture

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,7 +30,7 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
     const newTasks = tasks.map(task => task.id === id ? {
 
       ...task,
@@ -42,7 +42,7 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
     const taskFilter = tasks.filter(task => task.id !== id)
 
     setTasks(taskFilter);
